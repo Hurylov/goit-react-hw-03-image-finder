685f66d5de52df4fca4dabb7075de9ba29a5bbf2
Hurylov Dmitriy <dimarule123@gmail.com>
2023-02-06 14:45:10 +0200

    1

--- src/Service/api.jsx
+++ src/Service/api.jsx
@@ -1,4 +1,4 @@
-const API_KEY = '31271755-a238d7bfd5266bfb37ac3595c';
+const API_KEY = '33416486-c62635052b6b12d1fd4b33539';
 export function apiGet(name, page) {
   return fetch(
     `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&page=${page}&per_page=12`
